ef3d9c4727056c8252d08df5a8557f6d15f73cad
Zian <54756427+ZianDel@users.noreply.github.com>
2019-10-13 14:24:37 -0300

    Widget
    The picture does not appear on the app when it's on the home page, but the widget works

--- app/src/main/java/ar/com/sight/android/Sight_Widget.java
+++ app/src/main/java/ar/com/sight/android/Sight_Widget.java
@@ -14,25 +14,14 @@ public class Sight_Widget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            Intent sos= new Intent(context, SosActivity.class);
-            PendingIntent pendingSos= PendingIntent.getActivity(context, 0, sos, 0);
-            RemoteViews view= new RemoteViews(context.getPackageName(), R.layout.sight__widget);
-            view.setOnClickPendingIntent(R.id.widget_btnSos, pendingSos);
-            appWidgetManager.updateAppWidget(appWidgetId, view);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sight__widget);
+            views.setOnClickPendingIntent(R.id.widget_btnSos, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
-
