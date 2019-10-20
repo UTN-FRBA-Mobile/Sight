0db2d3125b6256854c93a70a8cc1c49ef78672cb
Zian <54756427+ZianDel@users.noreply.github.com>
2019-10-20 18:06:30 -0300

    Widget
    Picture still not fixed :,(

--- app/src/main/java/ar/com/sight/android/Sight_Widget.java
+++ app/src/main/java/ar/com/sight/android/Sight_Widget.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+/* import ar.com.sight.android.LoginActivity; If we have time, check the login first (or not we have to talk about it) */
 
 /**
  * Implementation of App Widget functionality.
@@ -15,7 +16,7 @@ public class Sight_Widget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, MainActivity.class);
+            Intent intent = new Intent(context, SosActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sight__widget);
