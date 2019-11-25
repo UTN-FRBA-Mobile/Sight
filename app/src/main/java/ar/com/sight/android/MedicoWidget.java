b3d3926d71b849bacb1ca5a4c92396cfaa4de8db
leanr <leandro.rozan@gmail.com>
2019-11-25 20:34:42 -0300

    Agrego Widgets para Robo y Em medica + SOS Arreglado mensaje alerta

--- app/src/main/java/ar/com/sight/android/MedicoWidget.java
+++ app/src/main/java/ar/com/sight/android/MedicoWidget.java
@@ -5,16 +5,17 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.view.Menu;
 import android.widget.RemoteViews;
 
-public class ExampleAppWidgetProvider extends AppWidgetProvider {
+public class MedicoWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, SosActivity.class);
+            Intent intent = new Intent(context, MenuPrincipalActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget_3);
             views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
--- app/src/main/java/ar/com/sight/android/RoboWidget.java
+++ app/src/main/java/ar/com/sight/android/RoboWidget.java
@@ -7,14 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-public class ExampleAppWidgetProvider extends AppWidgetProvider {
+public class RoboWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, SosActivity.class);
+            Intent intent = new Intent(context, MenuPrincipalActivity.class);
+
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget_2);
             views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
