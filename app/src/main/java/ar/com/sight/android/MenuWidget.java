cfe682f71bc9d4e3b8c3e6b44df183677de72d08
Zian <54756427+ZianDel@users.noreply.github.com>
2019-11-29 16:38:35 -0300

    Add Sos Widget & Small changes to existing widget
    My computer does not let me connect the app to internet. Can someone try to see if everything still works?

--- app/src/main/java/ar/com/sight/android/MenuWidget.java
+++ app/src/main/java/ar/com/sight/android/MenuWidget.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-public class ExampleAppWidgetProvider extends AppWidgetProvider {
+public class MenuWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -15,8 +15,8 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
-            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.menu_widget);
+            views.setOnClickPendingIntent(R.id.menu_widget_button, pendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
