48ded6879d380dd61a74d607af6ea5d302a76abd
leanr <leandro.rozan@gmail.com>
2019-11-29 23:06:32 -0300

    arreglo

--- app/src/main/java/ar/com/sight/android/SosWidget.java
+++ app/src/main/java/ar/com/sight/android/SosWidget.java
@@ -7,22 +7,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-/**
- * Implementation of App Widget functionality.
- */
-public class SosWidget extends AppWidgetProvider {
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, SosActivity.class);
+            Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sos_widget);
-            views.setOnClickPendingIntent(R.id.sos_widget_button, pendingIntent);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 }
-
