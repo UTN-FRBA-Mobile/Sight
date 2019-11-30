1f6da50c19fb2f2f88f99dcfd97d9895c1fdb7c3
leanr <leandro.rozan@gmail.com>
2019-11-29 23:31:36 -0300

    Arreglado lo que rompieron

---
+++ app/src/main/java/ar/com/sight/android/MedicoWidget.java
@@ -0,0 +1,24 @@
+package ar.com.sight.android;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.RemoteViews;
+
+public class MedicoWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MenuPrincipalActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget_3);
+            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
